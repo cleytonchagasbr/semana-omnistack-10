eba5f38deecc430beda59a64b2536b2c62715f68
Cleyton Chagas <cleyton-chagas@hotmail.com>
2020-01-27 22:00:53 -0300

    backend - Ok ,frontEnd - Ok, app - construindo

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+function Main() {
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } =  await requestPermissionsAsync();
+
+            if(granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+                
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if(!currentRegion) {
+        return null;
+    }
+
+    return (
+        <MapView initialRegion={currentRegion} style={style.map}>
+            <Marker coordinate={{ latitude: -23.5417528, longitude: -46.8278828 }}>
+                <Image source={{ uri: }}/>
+            </Marker>
+        </MapView>
+    ); 
+}
+
+const style = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+})
+
+export default Main;
